bda1dc2887dbb5970d3c1c0bdf7edcd08213174a
Hector Alejandro Merchan <hector.merchan@gmail.com>
2018-10-18 18:47:42 -0700

    Pur offline
    Added a new file to work with the dowloaded files. The functions seem to finally work. Pur.jl has not been updated with some changes I did in Pur offline

--- PUR_offline.jl
+++ PUR_offline.jl
@@ -4,22 +4,18 @@
 using CSV, DataFrames, DataFramesMeta
 using Gadfly
 using ZipFile
-using FTPClient
 #################################################################################
 
 #################################################################################
 ## Loading data from CDPR FTP server.                                          ##
 #################################################################################
 """
-    download_info(year1[, year2])
+    choose_years(year1[, year2])
 
-Saves the zip files from CDPR server into your computer memory
+Use this function to start analysis, by choosing the years you want to use for your study.
+This will create the proper filepaths.
 """
-function download_files(year1::Int, year2=Union{Nothing,Int})
-    ftp_init()
-    options = RequestOptions(hostname="transfer.cdpr.ca.gov/pub/outgoing/pur_archives/")
-    global files = DataFrame(year = [], files = [])
-
+function choose_years(year1::Int, year2=Union{Nothing,Int})
     if year2 != nothing
         first_year = minimum([year1, year2])
         second_year = maximum([year1, year2])
@@ -27,22 +23,21 @@ function download_files(year1::Int, year2=Union{Nothing,Int})
     else
         years = [year1]
     end
-
-    for i in 1:length(years)
-        name = "pur" * string(years[i]) *".zip"
-        resp = (year = years[i], files = ftp_get(options, name))
-        push!(files, resp)
-    end
-    ftp_cleanup()
-    return files
 end
 #################################################################################
 
 #################################################################################
 ## Unzipping and loading files into memory                                      ##
 #################################################################################
+function uncompress_file(year::Int)
+    path = "Pesticide_Use/pur" * string(year) *".zip"
+    io = open(path, "r")
+    io = IOBuffer(read(io))
+    fl = ZipFile.Reader(io)
+    return fl
+end
 """
-    get_files(year, kind)
+    get_files(:kind, year)
 
 Unizips and loads the desires files from the zip file from the year and save it in
 a Dataframe.
@@ -56,98 +51,114 @@ all counties in CA and their county_cd (codes).
 kind = :products loads the files "product.txt", which contains a list with all approved
 chemical products in CA for any year. The resulting dataframe contains
 """
-function get_files(year::Int, kind::Symbol)
-    for i in 1:size(files)[1]
-        if files.year[i] == 2016
-            reading = ZipFile.Reader(files.files[i].body)
-            for i in 1:length(reading.files)
-                if kind == :chemicals && reading.files[i].name == "pur2016/chemical.txt"
-                    chemicals = CSV.File(IOBuffer(read(reading.files[i])))
-                    return chemicals
-                    break
-                end
-                if kind == :sites && reading.files[i].name == "pur2016/site.txt"
-                    sites = CSV.File(IOBuffer(read(reading.files[i])))
-                    return sites
-                    break
-                end
-                if kind == :counties && reading.files[i].name == "pur2016/county.txt"
-                    counties = CSV.File(IOBuffer(read(reading.files[i])))
-                    return counties
-                    break
-                end
-                if kind == :products && reading.files[i].name == "pur2016/product.txt"
-                    products = CSV.File(IOBuffer(read(reading.files[i])))
-                    return products
-                    break
-                end
+function get_files(kind::Symbol, year::Int)
+    reading = uncompress_file(year)
+    # the zile file for 2016 has and extra "pur2016/" on the file name that has to be removed
+    if year == 2016
+        for i in 1:length(reading.files)
+            (Y, N) = split(reading.files[i].name, "/")
+            reading.files[i].name = N
+        end
+    end
+
+    for i in 1:length(reading.files)
+        if kind == :chemicals && reading.files[i].name == "chemical.txt"
+            fl = CSV.read(IOBuffer(read(reading.files[i])))
+            return fl
+        elseif kind == :sites && reading.files[i].name == "site.txt"
+            fl = CSV.read(IOBuffer(read(reading.files[i])))
+            return name
+        elseif kind == :counties && reading.files[i].name == "county.txt"
+            fl = CSV.read(IOBuffer(read(reading.files[i])))
+            return fl
+        elseif kind == :products && reading.files[i].name == "product.txt"
+            fl = CSV.read(IOBuffer(read(reading.files[i])))
+            return fl
+        end
+    end
+    throw("Wrong symbol for kind")
+end
+
+# experimental function. need to time with BenchmarkTools
+function get_files1(kind::Symbol, year::Int)
+    reading = uncompress_file(year)
+    # the zile file for 2016 has and extra "pur2016/" on the file name that has to be removed
+    if year == 2016
+        for i in 1:length(reading.files)
+            (Y, N) = split(reading.files[i].name, "/")
+            reading.files[i].name = N
+        end
+    end
+
+    if kind == :chemicals
+        for i in 1:length(reading.files)
+            if reading.files[i].name == "chemical.txt"
+                name = CSV.read(IOBuffer(read(reading.files[i])))
+                return name
             end
         end
-
-        if files.year[i] == year
-            reading = ZipFile.Reader(files.files[i].body)
-            for i in 1:length(reading.files)
-                if kind == :chemicals && reading.files[i].name == "chemical.txt"
-                    chemicals = CSV.File(IOBuffer(read(reading.files[i])))
-                    return chemicals
-                    break
-                end
-                if kind == :sites && reading.files[i].name == "site.txt"
-                    sites = CSV.File(IOBuffer(read(reading.files[i])))
-                    return sites
-                    break
-                end
-                if kind == :counties && reading.files[i].name == "county.txt"
-                    counties = CSV.File(IOBuffer(read(reading.files[i])))
-                    return counties
-                    break
-                end
-                if kind == :products && reading.files[i].name == "product.txt"
-                    products = CSV.File(IOBuffer(read(reading.files[i])))
-                    return products
-                    break
-                end
+    elseif kind == :sites
+        for i in 1:length(reading.files)
+            if reading.files[i].name == "site.txt"
+                name = CSV.read(IOBuffer(read(reading.files[i])))
+                return name
+            end
+        end
+    elseif kind == :counties
+        for i in 1:length(reading.files)
+            if reading.files[i].name == "county.txt"
+                name = CSV.read(IOBuffer(read(reading.files[i])))
+                return name
+            end
+        end
+    elseif kind == :products
+        for i in 1:length(reading.files)
+            if reading.files[i].name == "product.txt"
+                name = CSV.read(IOBuffer(read(reading.files[i])))
+                return name
             end
         end
     end
+    throw("Wrong symbol for kind")
 end
 
 # Function to extract the differences between the approved chemicals between years
-function compare_chem_years(year1::Int, year2::Int)
-    join(get_files(maximum([year1, year2]), :chemicals),
-         get_files(minimum([year1, year2]), :chemicals),
-         on =:chem_code, kind = :anti)
-end
-
-# Function to extract the differences between the approved sites between years.
-function compare_site_years(year1::Int, year2::Int)
-    join(get_files(maximum([year1, year2]), :sites),
-         get_files(minimum([year1, year2]), :sites),
-         on =:site_code, kind = :anti)
-end
-
-function compare_product_years(year1::Int, year2::Int)
-    join(get_files(maximum([year1, year2]), :products),
-         get_files(minimum([year1, year2]), :products),
-         on =:site_code, kind = :anti)
+function compare_info_years(kind::Symbol, year1::Int, year2::Int)
+    if kind == :chemicals
+        join(get_files(maximum([year1, year2]), :chemicals),
+            get_files(minimum([year1, year2]), :chemicals),
+            on =:chem_code, kind = :anti)
+    elseif kind == :sites
+        join(get_files(maximum([year1, year2]), :sites),
+             get_files(minimum([year1, year2]), :sites),
+             on =:site_code, kind = :anti)
+    elseif kind == :products
+        join(get_files(maximum([year1, year2]), :products),
+             get_files(minimum([year1, year2]), :products),
+             on =:prodno, kind = :anti)
+    else throw("Wrong symbol for kind")
+    end
 end
 
 # find sites using any word (site) as a string.
 function find_product_code(product::AbstractString, year::Int)
-    products = get_files(year, :products)
-    filtered = DataFrame(prod=[], site_name=[])
-    for i in 1:size(sites,1)
-        matching = match(Regex(uppercase(site)), sites[i,2])
+    products = get_files(:products, year)
+    filtered = DataFrame(prodno=[], product_name=[])
+    for i in 1:size(products,1)
+        matching = match(Regex(uppercase(product)), products[i,8])
         if matching != nothing
-            push!(filtered, (site_code = sites[i,1], site_name=sites[i,2]))
+            push!(filtered, (prodno = products[i,1], product_name=products[i,8]))
         end
     end
-    return filtered
+    if nrow(filtered) != 0
+        return filtered
+    else throw("Non-existent product")
+    end
 end
 
 # find sites using any word (site) as a string.
 function find_site_code(site::AbstractString, year::Int)
-    sites = get_files(year, :sites)
+    sites = get_files(:sites, year)
     filtered = DataFrame(site_code=[], site_name=[])
     for i in 1:size(sites,1)
         matching = match(Regex(uppercase(site)), sites[i,2])
@@ -155,20 +166,37 @@ function find_site_code(site::AbstractString, year::Int)
             push!(filtered, (site_code = sites[i,1], site_name=sites[i,2]))
         end
     end
-    return filtered
+    if nrow(filtered) != 0
+        return filtered
+    else throw("Non-existent commodity")
+    end
+end
+
+function find_chem_code(chemical::AbstractString, year::Int)
+    chemicals = get_files(:chemicals, year)
+    filtered = DataFrame(chem_code=[], chem_name=[])
+    for i in 1:size(chemicals,1)
+        matching = match(Regex(uppercase(chemical)), chemicals[i,3])
+        if matching != nothing
+            push!(filtered, (chem_code = chemicals[i,1], chem_name=chemicals[i,3]))
+        end
+    end
+    if nrow(filtered) != 0
+        return filtered
+    else throw("Non-existent chemical")
+    end
 end
 
 # Extract the county code
 function find_county_code(county_name::AbstractString, year::Int)
     counties = get_files(year, :counties)
-    county_code = []
     for i in 1:length(counties[:couty_name])
         if counties[i,2] == uppercase(county_name)
             county_code = counties[i,1]
-            break
+            return county_code
         end
     end
-    return county_code
+    throw("Wrong county name")
 end
 
 ################################################################################
@@ -177,32 +205,24 @@ end
 # Function to load pur file from disk, using the county name as a string and the year
 function load_pur(county_name::AbstractString, year::Int, dropmissing::Bool = true)
     county_code = find_county_code(county_name, year)
-
     string_year = string(year)
 
-    pur = ()
-    for i in 1:length(files.files)
-        if files.year[i] == 2016
-            reading = ZipFile.Reader(files.files[i].body)
-            for i in 1:length(reading.files)
-                if reading.files[i].name == "pur2016/udc" * string_year[3:4] * "_"* string(county_code) * ".txt"
-                    pur = CSV.File(IOBuffer(read(reading.files[i])), escapechar='#',
-                    types=Dict(18 => Union{Int64, Char, Missing},20 => Union{Int64, Char, Missing}))
-                end
-            end
-        end
-        if files.year[i] == year
-            reading = ZipFile.Reader(files.files[i].body)
-            for i in 1:length(reading.files)
-                if reading.files[i].name == "udc" * string_year[3:4] * "_"* string(county_code) * ".txt"
-                    pur = CSV.File(IOBuffer(read(reading.files[i])), escapechar='#',
-                    types=Dict(18 => Union{Int64, Char, Missing},20 => Union{Int64, Char, Missing}))
-                end
-            end
+    reading = uncompress_file(year)
+    # the zip file for 2016 has and extra "pur2016/" on the file name that has to be removed
+    if year == 2016
+        for i in 1:length(reading.files)
+            (Y, N) = split(reading.files[i].name, "/")
+            reading.files[i].name = N
         end
     end
 
-    pur_df = DataFrame(pur)
+    for i in 1:length(reading.files)
+        if reading.files[i].name == "udc" * string_year[3:4] * "_"* string(county_code) * ".txt"
+            pur = CSV.File(IOBuffer(read(reading.files[i])), escapechar='#',
+            types=Dict(18 => Union{Int64, Char, Missing},20 => Union{Int64, Char, Missing}))
+            pur_df = DataFrame(pur)
+        end
+    end
 
     pur_df = @select(pur_df, :use_no, :prodno, :chem_code, :prodchem_pct, :lbs_chm_used,
     :lbs_prd_used, :amt_prd_used, :unit_of_meas, :acre_planted, :unit_planted,
@@ -238,9 +258,15 @@ function select_site_code(df::DataFrame, site_code::Int)
     @where(df, :site_code .== site_code)
 end
 
-function select_insecticide(df::DataFrame, insecticide_name::AbstractString)
-    insecticide_name = string(uppercase(insecticide_name))
-    @where(df, :chem_name .== insecticide_name)
+function select_insecticide(df::DataFrame, insecticide_name::Union{AbstractString, Int})
+    if typeof(insecticide_name)== AbstractString
+        insecticide_name = string(uppercase(insecticide_name))
+        @where(df, :chem_name .== insecticide_name)
+    elseif typeof(insecticide_name)==Int
+        df = dropmissing!(df)
+        @where(df, :chem_code .== insecticide_name)
+    else throw("Wrong insecticide name or code")
+    end
 end
 
 # Function to append the data from different years
@@ -270,7 +296,7 @@ end
 
 # Function to obtain the information in insecticide use from a given commodity and county
 function insecticide_use(county_name::AbstractString,
-                         insecticide_name::AbstractString,
+                         insecticide_name::Union{AbstractString, Int},
                          site_code::Int,
                          first_year::Int, last_year::Union{Nothing,Int},
                          dropmissing::Bool=true)
@@ -284,7 +310,7 @@ function insecticide_use(county_name::AbstractString,
     return total
 end
 
-function plot_insecticide_use(ounty_name::AbstractString,
+function plot_insecticide_use(county_name::AbstractString,
                          insecticide_name::AbstractString,
                          site_code::Int,
                          first_year::Int, last_year::Union{Nothing,Int},
@@ -293,3 +319,5 @@ function plot_insecticide_use(ounty_name::AbstractString,
                              dropmissing)
     plot(result, x = :year, y = :lbs_chm_used, Geom.line)
 end
+
+plot(abamectin_monterey_154, x = :year, y = :lbs_chm_used, Geom.line, Theme(background_color="white"))
